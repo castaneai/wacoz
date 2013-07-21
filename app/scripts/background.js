b35fea2970d4586636722cec38892acf88c33294
castaneai <castaneai@castaneai.net>
2013-07-21 18:58:39 +0900

    ニコニコ生放送の番組表を取ってくるところまで作成

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,5 +1,11 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-    console.log('previousVersion', details.previousVersion);
-});
+var preservedLives = {};
+
+function preserve(liveId, startTime) {
+    preservedLives[liveId] = startTime;
+}
+
+function cancel(liveId) {
+
+}
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function MainController($scope, $http) {
+    var url = 'http://live.nicovideo.jp/api/getZeroTimeline?date=2013-07-21';
+    $http.get(url).then(function(response) {
+        $scope.lives = _.filter(response.data.timeline.stream_list, function(live) {
+            return live.status == 'comingsoon' && live.provider_type == 'official';
+        });
+    });
+}
